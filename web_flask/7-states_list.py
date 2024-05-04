e3cae39cb757ab0a5e5a9564032e2ad24ab40050
jedidia-nku <jedinkuze123@gmail.com>
2024-05-04 23:51:04 +0000

    task completed

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
-"""
-web_flask/7-states_list.py
-"""
-from flask import Flask, render_template
-from models import storage
-from models.state import State
+"""Starts a Flask web application.
 
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /states_list: HTML page with a list of all State objects in DBStorage.
+"""
+from models import storage
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route("/states_list", strict_slashes=False)
-def state_list():
-    """Prints list of all states in sorted order"""
-    states = storage.all(State)
+def states_list():
+    """Displays an HTML page with a list of all State objects in DBStorage.
+
+    States are sorted by name.
+    """
+    states = storage.all("State")
     return render_template("7-states_list.html", states=states)
 
 
-# Define a function to handle teardown
 @app.teardown_appcontext
-def teardown_appcontext(self):
-    """close session context"""
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000", debug=True)
+    app.run(host="0.0.0.0")
