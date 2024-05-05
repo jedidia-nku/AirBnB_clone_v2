728cf78fc2198a756628d2a95c5ebf9e6e2a317c
jedidia-nku <jedinkuze123@gmail.com>
2024-05-05 01:22:00 +0000

    Done

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,32 +1,35 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
+'''A simple Flask web application.
+'''
+from flask import Flask, render_template
 
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /cities_by_states: HTML page with a list of all states and related cities.
-"""
 from models import storage
-from flask import Flask
-from flask import render_template
+from models.state import State
+
 
 app = Flask(__name__)
+'''The Flask application instance.'''
+app.url_map.strict_slashes = False
 
 
-@app.route("/cities_by_states", strict_slashes=False)
+@app.route('/cities_by_states')
 def cities_by_states():
-    """Displays an HTML page with a list of all states and related cities.
-
-    States/cities are sorted by name.
-    """
-    states = storage.all("State")
-    return render_template("8-cities_by_states.html", states=states)
+    '''The cities_by_states page.'''
+    all_states = list(storage.all(State).values())
+    all_states.sort(key=lambda x: x.name)
+    for state in all_states:
+        state.cities.sort(key=lambda x: x.name)
+    ctxt = {
+        'states': all_states
+    }
+    return render_template('8-cities_by_states.html', **ctxt)
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def flask_teardown(exc):
+    '''The Flask app/request context end event listener.'''
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
